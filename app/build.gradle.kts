d68349e24e4eaee71d1805dd78c100bd9d2482a2
socroode <socroode.t@gmail.com>
2026-09-22 01:25:08 -1000

    Build Shizzi v3 with upstream auto-recovery

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,8 +60,8 @@ android {
 
         minSdk = 30
         targetSdk = 35
-        versionCode = 7
-        versionName = "0.4.0-rc.4-40x5-hotspot"
+        versionCode = 8
+        versionName = "0.4.0-v3-40x5-hotspot"
 
         buildConfigField("int", "SERVICE_BUILD_ID", "${sourceFingerprint(projectDir)}")
 
