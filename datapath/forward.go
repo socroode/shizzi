075d644de86eb4bf081534247c7c04ee0c0e7b2b
socroode <socroode.t@gmail.com>
2026-09-24 11:59:33 -1000

    Route live-usage HTTP through reserved peer address

--- datapath/forward.go
+++ datapath/forward.go
@@ -59,7 +59,7 @@ func forwardTCP(request *tcp.ForwarderRequest, dialer *net.Dialer, traffic *Traf
 	clientIP := sourceOf(id)
 
 	if id.LocalPort == 80 && traffic != nil &&
-		(traffic.portalRequiredFor(clientIP) || addressString(id.LocalAddress) == "192.0.2.2") {
+		(traffic.portalRequiredFor(clientIP) || addressString(id.LocalAddress) == portalStatusAddress) {
 		var queue waiter.Queue
 		endpoint, tcpipErr := request.CreateEndpoint(&queue)
 		if tcpipErr != nil {
