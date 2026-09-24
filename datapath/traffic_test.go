37b59e9cb9ad9181ad1a2133206fd46e0d51e1ee
socroode <socroode.t@gmail.com>
2026-09-24 11:59:53 -1000

    Restore shared TUN-address coverage in traffic tests

--- datapath/traffic_test.go
+++ datapath/traffic_test.go
@@ -70,7 +70,7 @@ func TestResetStatsKeepsPolicy(t *testing.T) {
 func TestTunnelAddressesStayOutOfClientList(t *testing.T) {
 	m := newTrafficManager()
 
-	m.account("192.0.2.1", directionDownload, 100)
+	m.account("192.0.2.2", directionDownload, 100)
 	m.account("2001:db8::2", directionUpload, 50)
 
 	var snapshot trafficStatsSnapshot
@@ -94,12 +94,12 @@ func TestSharedPolicyBlocksNatTraffic(t *testing.T) {
 	m := newTrafficManager()
 	m.setSharedPolicy(ClientPolicy{QuotaBytes: 100})
 
-	if !m.waitAllowed("192.0.2.1", directionDownload, 100) {
+	if !m.waitAllowed("192.0.2.2", directionDownload, 100) {
 		t.Fatal("shared traffic should be allowed up to quota")
 	}
 	m.account("192.0.2.1", directionDownload, 100)
 
-	if m.waitAllowed("192.0.2.1", directionDownload, 1) {
+	if m.waitAllowed("192.0.2.2", directionDownload, 1) {
 		t.Fatal("shared NAT traffic should stop after quota")
 	}
 }
