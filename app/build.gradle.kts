ff3e148352fd485a699fd9972fd7d729f14bda8e
socroode <socroode.t@gmail.com>
2026-09-24 21:49:43 -1000

    Bump Shizzi to 1.7.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,8 +60,8 @@ android {
 
         minSdk = 30
         targetSdk = 35
-        versionCode = 29
-        versionName = "1.7.0"
+        versionCode = 30
+        versionName = "1.7.1"
 
         buildConfigField("int", "SERVICE_BUILD_ID", "${sourceFingerprint(projectDir)}")
 
