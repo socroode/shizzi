c08b26c03bcdde2ccdc5abd8bdf700c3e561e4a5
socroode <socroode.t@gmail.com>
2026-09-26 05:25:15 -1000

    Bump Shizzi to 1.7.10

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,8 +62,8 @@ android {
 
         minSdk = 30
         targetSdk = 35
-        versionCode = if (migrationBuild) 39 else 40
-        versionName = if (migrationBuild) "1.7.9-migration" else "1.7.9"
+        versionCode = if (migrationBuild) 40 else 41
+        versionName = if (migrationBuild) "1.7.10-migration" else "1.7.10"
 
         buildConfigField("int", "SERVICE_BUILD_ID", "${sourceFingerprint(projectDir)}")
 
